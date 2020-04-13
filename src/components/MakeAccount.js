a95e4dec7b4311572996a29451542b5ffefc4a8f
nvaidyamath <nuv200@nyu.edu>
2020-04-13 01:48:41 -0700

    set up google authentication for sign up, and alerts if sign up is unsuccesful

--- src/components/MakeAccount.js
+++ src/components/MakeAccount.js
@@ -13,6 +13,22 @@ import {
 } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 import autoBind from 'react-autobind';
+import * as firebase from 'firebase';
+
+// scout's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDPwe1ORI1Orf3A3dNY9NPzZEDmbMc1Va4",
+    authDomain: "scout-32fb7.firebaseapp.com",
+    databaseURL: "https://scout-32fb7.firebaseio.com",
+    projectId: "scout-32fb7",
+    storageBucket: "scout-32fb7.appspot.com",
+    messagingSenderId: "1049255843674",
+    appId: "1:1049255843674:web:5538988d4fd902c74e0563",
+    measurementId: "G-92FDWSF58L"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();  //firebase authentication
 
 const options = [
   { key: 'm', text: 'Male', value: 'male' },
@@ -47,10 +63,30 @@ export default class Login extends React.Component {
 
   }
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
 
   handleSubmit = () => {
-    console.log(this.state.username, this.state.email, this.state.gender);
+  auth.createUserWithEmailAndPassword(this.state.email,this.state.password).catch(function(error) {    //create authentication
+  // Handle Errors here.
+  var errorMessage = error.message;
+  if (errorMessage != null) {
+    alert(errorMessage);
+    return;
+  }
+  else {
+    alert("Successfully signed up!");
+  }
+  });
+
+    firebase.database().ref('users/' + this.state.username).set({  //store user data
+      email: this.state.email,
+      first_name: this.state.first_name,
+      last_name:this.state.last_name,
+      gender:this.state.gender,
+      type:this.state.type,
+      experience:this.state.experience
+     });
+    //console.log(this.state.username, this.state.email, this.state.gender);
   }
 
   componentDidMount() {
