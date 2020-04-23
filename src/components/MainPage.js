74920a76f475e0ca1cbceb4570373c022691b3f7
Sabrena Beck <sb5829@nyu.edu>
2020-04-22 22:18:58 -0400

    included search bar

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,8 @@
 import React, { Component} from 'react';
+import PropTypes from 'prop-types'
 import HeaderApp from '../Header';
 import _ from 'lodash'
-import { Button, Icon, Card, Modal, Header, Segment, Grid, Search} from 'semantic-ui-react';
+import { Button, Icon, Card, Modal, Header, Segment, Grid, Search, Label} from 'semantic-ui-react';
 import { withRouter, Link } from 'react-router-dom';
 import picture from '../images/artist1.jpg';
 import picture2 from '../images/artist2.png';
@@ -26,39 +27,64 @@ const Profile = (props) => (
 
   </div>
 )
+const resultRenderer = ({ username }) => <Link to={'/'+username} >{username}</Link>
+resultRenderer.propTypes = {
+  username: PropTypes.string,
+}
 
 class SearchBarComponent extends Component {
 
+
   state = {isLoading: false, results:[], value:""}
-  handleResultSelect = (e, { result }) => this.setState({ value: result.username })
+
+  handleResultSelect = (e, { result }) =>{
+    this.setState({ value: result.username })
+
+  }
+
 
   handleSearchChange = (e, { value }) => {
+    console.log("props!!!!", this.props.users)
+
     this.setState({ isLoading: true, value })
 
     setTimeout(() => {
       if (this.state.value.length < 1) return this.setState({isLoading: false, results:[], value: "" })
 
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+
       const isMatch = (result) => re.test(result.username)
       this.setState({
         isLoading: false,
-        results: _.filter(this.state.users, isMatch),
+        results: _.filter(this.props.users, isMatch),
+
       })
+      console.log("result is", this.state.results)
     }, 300)
   }
 
   render()
   {
+    const { isLoading, value, results } = this.state
     return (
   <div className="searchBar" container sytle = {{marginTop: 30, display: 'flex', justifyContent: 'center'}}>
-  <Search
-    loading={this.state.isLoading}
-    onResultSelect={this.handleResultSelect}
-    onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true, })}
-    results={this.state.results}
-    value={this.state.value}
-    {...this.props}
-  />
+  <Grid>
+        <Grid.Column width={6}>
+          <Search
+            loading={isLoading}
+            onResultSelect={this.handleResultSelect}
+            onSearchChange={_.debounce(this.handleSearchChange, 500, {
+              leading: true,
+            })}
+            results={results}
+            resultRenderer={resultRenderer}
+            value={value}
+            {...this.props}
+
+          />
+        </Grid.Column>
+
+      </Grid>
   </div>
 )
 }
@@ -107,24 +133,39 @@ class MainPage extends Component {
     this.state = {
       users: {},
       profile_pics: {},
+      usernames:[],
       currentUser:''
     };
   }
 
   componentDidMount() {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        await this.getUserData();
+        await this.getProPics();
+      }
+    })
+
+  }
+  wait(ms) {
+  return new Promise(r => setTimeout(r, ms));
+  }
+  getUserData = async (e) => {
     var that = this;
     var usernames='';
+    var usernames={};
     var user;
     var attr;
-     db.ref('users/').on('value',function(snapshot) {
+    if (auth.currentUser){
+      db.ref('users/').on('value',function(snapshot) {
         usernames = snapshot.val();
+
         that.setState({
           users: usernames
         });
         for(user of Object.keys(that.state.users)){
-
+          that.state.usernames.push({"username":user});
           for(attr of Object.keys(that.state.users[user])){
-
             if(attr=='type'){
               if (that.state.users[user]['type']==1){
                 that.state.users[user]['type']='Scout';
@@ -133,50 +174,29 @@ class MainPage extends Component {
                 that.state.users[user]['type']='Recruiter';
               }
             }
-            if(attr=='email' && that.state.users[user][attr]==auth.currentUser.email){
-              that.setState({currentUser: user});
-            }
           }
         }
-        that.getProPics();
-     });
-
-
-
-  }
+      });
+    }
+    await this.wait(500);
+ }
 
   getProPics = async (e) => {
-
-    if (auth.currentUser){
-      var pics= {}
-      var user;
-      for(user of Object.keys(this.state.users)){
-        var storageRef = storage.ref().child('files/'+this.state.users[user]['email']+'/profilepics');
+    var pics={}
+    var user;
+    for (user of Object.keys(this.state.users)){
+      var storageRef = storage.ref().child('files/'+this.state.users[user]['email']+'/profilepics');
         //var listRef = storage.ref().child('files/'+auth.currentUser.email+'/gallery'); //all user files
         var res = await storageRef.listAll();
         for(var itemRef of res.items){
-          // console.log("inside storage ref", user, itemRef.getDownloadURL());
+          console.log("inside storage ref", user, itemRef.getDownloadURL());
           var url = await itemRef.getDownloadURL();
-          // console.log("user is", user, url);
+          console.log("user is", user, url);
           pics[user]=url;
-
         }
-      }
-
-      this.setState({profile_pics:pics});
+        this.setState({profile_pics:pics});
     }
-  }
-
-  submitFunction = () => {
-    auth.signOut().then(() => {
-      this.props.history.push('Login')
-
-  // Sign-out successful.
-  }).catch(function(error) {
-    alert(error.message)
-    // console.log("fail");
-  // An error happened.
-  });
+     await  this.wait(500);
   }
 
   uploadFunction = () => {
@@ -278,9 +298,15 @@ class MainPage extends Component {
             placeContent: 'end space-between'
           }}>
             <Profile user={this.state.currentUser}/>
-            <Button type='submit' onClick={this.submitFunction}>Log Out</Button>
+            <Button type='submit' onClick={this.submitFunction} basic color='teal'style={{width:100, height:50}}>Log Out</Button>
           </div>
           <HeaderApp />
+          <div style={{display:'flex', justifyContent:'center', marginTop:'10px'}}>
+          <br/>
+          <SearchBarComponent users={this.state.usernames} />
+          </div>
+
+
         </div>
 
         <div>
